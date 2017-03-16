a7edbf6984f990c360c3177a7aa8930b76cf4533
mariusfelea <mariusfelea23@gmail.com>
2017-03-16 15:03:30 +0200

    Commit with jenkins

---
+++ src/test/java/WelcomeControllerTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class WelcomeControllerTest {
+	
+	@Test
+	public void sum() {
+		int first = 1;
+		int second = 2;
+		
+		int sum = 3;
+		
+		assertTrue("Error, random is too high", sum == first + second);
+	}
+
+}
